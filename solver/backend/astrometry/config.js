4997a08e5107f05be12438b9b2d459869f96129f
陈曦 <chenxi@chenxideMacBook-Pro.local>
2021-06-29 10:58:42 +0800

    move variable to config file

---
+++ solver/backend/astrometry/config.js
@@ -0,0 +1,9 @@
+const config = {
+    BASE_URL: 'http://nova.astrometry.net',
+    API_KEY: 'hopttfyedswulqlv',
+    STATUS_SUCCESS: 'success',
+    STATUS_ERROR: 'error',
+    API_LOGIN: '/api/login',
+}
+
+export default config
