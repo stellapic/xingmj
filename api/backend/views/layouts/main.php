4fb36e4c553b6f1655bc285755118a713766728d
Eson <helloeson@gmail.com>
2021-07-04 16:03:36 +0800

    admin page

--- api/backend/views/layouts/main.php
+++ api/backend/views/layouts/main.php
@@ -134,7 +134,7 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
-          <i class="fas fa-th-large"></i>
+          <i class="fas fa-sign-out-alt"></i>
         </a>
       </li>
     </ul>
