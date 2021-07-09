c40012c44ab07887859a46c936583e8b5bfb13d7
Eson <helloeson@gmail.com>
2021-07-10 00:30:20 +0800

    go solver

--- backend/goes/solverQueueListen.go
+++ backend/goes/solverQueueListen.go
@@ -3,12 +3,10 @@ package main
 import (
     "database/sql"
     // "io"
-    "io/ioutil"
+    // "io/ioutil"
 
     "fmt"
     // "net/http"
-
-    _ "github.com/go-sql-driver/mysql"
 
     "log"
     "os"
@@ -16,20 +14,160 @@ import (
     "syscall"
     "time"
     "context"
+
+    jsoniter "github.com/json-iterator/go"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/go-redis/redis/v8"
+
 )
+
+const (
+	QUEUE_NAME = "queue:solver:done"
+	REDIS_ADDR = "localhost:6379"
+)
+
+type solverResult struct {
+	id int
+	status string
+	annotatedImage string
+	gridImage string
+	result string
+}
 
 var ctx = context.Background()
 
  
 func main() {
-    daemon(0, 1)
-    for {
-        fmt.Println(scan())
-        time.Sleep(1 * time.Second)
-    }
- 
+    // daemon(0, 1)
+    listenRedis()
 }
 
+func listenRedis() {
+	client := redis.NewClient(&redis.Options{
+        Addr: REDIS_ADDR,
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    })
+    // lpop
+    for {
+    	lPop := client.LPop(ctx, QUEUE_NAME)
+	    if lPop.Err() != nil {
+	        fmt.Println(lPop.Err())
+	    }
+	    lPopVal := lPop.Val()
+	    if lPopVal != "" {
+		    fmt.Println("message", lPopVal)
+		    // go handle message
+		    id, status, annotatedImage, gridImage := parseJson([]byte(lPopVal))
+		    result := solverResult{
+				id:     id,
+				status:   status,
+				annotatedImage: annotatedImage,
+				gridImage:   gridImage,
+				result: lPopVal,
+			}
+		    // ...
+		    saveToDb(result)
+		    fmt.Println("id ", id, "work done.")
+	    }
+	    // should use goroutine.
+	    time.Sleep(1 * time.Second)
+	}
+}
+
+
+func saveToDb(result solverResult) (string) {
+	b, err := ioutil.ReadFile("/data/secrets/mysql.txt") // just pass the file name
+    if err != nil {
+        fmt.Print(err)
+    }
+    connectionStr := string(b)
+    fmt.Println(connectionStr)
+
+	db, err := sql.Open("mysql", "xingmj:123456@tcp(xingmj:3306)/xingmj")
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
+
+	// Prepare statement for reading data
+	stmtOut, err := db.Prepare("SELECT image FROM photo WHERE id = ?")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer stmtOut.Close()
+
+	var tagName string // we "scan" the result in here
+	// Query another number.. 2 maybe?
+	err = stmtOut.QueryRow(result.id).Scan(&tagName) // WHERE id = 2
+
+	fmt.Println("image: ", tagName)
+
+	//-------5、修改数据--------  
+	result2, err := db.Exec("update photo set solver_result=?,graph_resolve=?,graph_position=?,update_at=? where id = ?", result.result, result.annotatedImage, result.gridImage, time.Now(), result.id)
+	if err != nil {  
+	   fmt.Println("修改数据错误", err)  
+	   return ""
+	}  
+	i2, _ := result2.RowsAffected() //受影响行数  
+	fmt.Printf("受影响行数：%d \n", i2)
+
+	return "image: "+tagName
+}
+
+func parseJson(jsonVal []byte) (int, string, string, string) {
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+    var result map[string]interface{}
+    var annotatedImage string
+    var gridImage string
+	json.Unmarshal(jsonVal, &result)
+	// print_map(result)
+	// panic("exit")
+	id := int(result["id"].(float64))
+	status := result["status"].(string)
+	if status == "success" {
+		// status = "yes we success"
+		annotated := result["annotated"].(map[string]interface{})
+		for key, value := range annotated {
+			if key == "display" {
+				annotatedImage = value.(string)
+			}
+		}
+		grid := result["grid"].(map[string]interface{})
+		for k, v := range grid {
+			if k == "display" {
+				gridImage = v.(string)
+			}
+		}
+	}
+	fmt.Println(id, status, annotatedImage, gridImage)
+	return id, status, annotatedImage, gridImage
+}
+
+func print_map(m map[string]interface{}) {
+    for k, v := range m {
+        switch value := v.(type) {
+        case nil:
+            fmt.Println(k, "is nil", "null")
+        case string:
+            fmt.Println(k, "is string", value)
+        case int:
+            fmt.Println(k, "is int", value)
+        case float64:
+            fmt.Println(k, "is float64", value)
+        case []interface{}:
+            fmt.Println(k, "is an array:")
+            for i, u := range value {
+                fmt.Println(i, u)
+            }
+        case map[string]interface{}:
+            fmt.Println(k, "is an map:")
+            print_map(value)
+        default:
+            fmt.Println(k, "is unknown type", fmt.Sprintf("%T", v))
+        }
+    }
+}
 
 func daemon(nochdir, noclose int) int {
     var ret, ret2 uintptr
@@ -90,35 +228,6 @@ func daemon(nochdir, noclose int) int {
     }
  
     return 0
-}
-
-
-func scan() (string) {
-	b, err := ioutil.ReadFile("/data/secrets/mysql.txt") // just pass the file name
-    if err != nil {
-        fmt.Print(err)
-    }
-    connectionStr := string(b)
-    fmt.Println(connectionStr)
-
-	db, err := sql.Open("mysql", connectionStr)
-	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-	}
-	defer db.Close()
-
-	// Prepare statement for reading data
-	stmtOut, err := db.Prepare("SELECT tag_title FROM photo_tag WHERE id = ?")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	defer stmtOut.Close()
-
-	var tagName string // we "scan" the result in here
-	// Query another number.. 2 maybe?
-	err = stmtOut.QueryRow(2).Scan(&tagName) // WHERE id = 2
-
-	return "The tag name is "+tagName
 }
 
 
