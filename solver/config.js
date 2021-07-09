4cf0ce0a88462f05707196d4c12a17b24e75341a
陈曦 <chenxi@chenxideMacBook-Pro.local>
2021-07-09 21:41:22 +0800

    add grid/retry options

--- solver/config.js
+++ solver/config.js
@@ -3,15 +3,11 @@
 const config = {
     log: {
         LOG_LEVEL: 'trace', // fatal/error/warn/info/debug/trace
-        LOG_SRC: false, // do not set true in production env
+        LOG_SRC: true, // do not set true in production env
     },
     solver: 'astrometry', // astrometry | pi
     timeout: 10000, // 10s
-    retry: {
-        RETRIES: 20,
-        MIN_TIMEOUT: 10000, // 10s
-        MAX_TIMEOUT: 20000 // 20s
-    },
+    types: ['full', 'display'],
     api: {
         BASE_URL: 'http://nova.astrometry.net',
         API_KEY: 'hopttfyedswulqlv',
@@ -23,7 +19,13 @@ const config = {
         API_JOB_INFO: `/api/jobs/{jobid}/info`,
         API_ANNOTATED_FULL: '/annotated_full',
         API_ANNOTATED_DISPLAY: '/annotated_display',
-        API_SKY_PLOT: 'sky_plot/zoom'
+        API_SKY_PLOT: '/sky_plot/zoom',
+        API_GRID: '/grid_display',
+        retry: {
+            RETRIES: 40,
+            MIN_TIMEOUT: 10000, // 10s
+            MAX_TIMEOUT: 20000 // 20s
+        }
     },
     annotated: '/Users/chenxi/Desktop/imufu/xingmj/solver/test/annotated_images',
     redis: {
@@ -31,7 +33,14 @@ const config = {
             port: 13697,
             host: 'redis-13697.c275.us-east-1-4.ec2.cloud.redislabs.com',
             password: '19810704',
-            db: 0
+            db: 0,
+            lazyConnect: true,
+            showFriendlyErrorStack: true
+        },
+        retry: {
+            RETRIES: 3,
+            MIN_TIMEOUT: 1000, // 500ms
+            MAX_TIMEOUT: 2000 // 2s
         },
         TIMEOUT_INDEFINITELY: 0
     }
